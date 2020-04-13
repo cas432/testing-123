2270ac94d4f7e20f4de72ac06a5fbec856a81071
cas432 <cas432@georgetown.edu>
2020-04-13 11:57:54 -0400

    configure

---
+++ my_script.py
@@ -0,0 +1,12 @@
+def enlarge(i):
+    return i * 100
+
+if __name__ == "__main__":
+    #only run if invoked from the command line
+    #not if imported by another file
+    
+    my_number = float(input("please input a number: "))
+    n = enlarge(my_number)
+    print(n)
+
+# cannot be in global scope
